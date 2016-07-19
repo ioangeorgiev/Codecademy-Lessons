2a05c86f64afd980d602b630d3128c627c2c15ba
Йоан Георгиев <karattekid12@abv.bg>
2016-07-19 04:04:58 +0300

    Loops and New Dragon Game

---
+++ JavaScript-Lessons/loops.js
@@ -0,0 +1,104 @@
+/*====== For Loops ======*/
+
+/*for (var counter = 1; counter < 10; counter++) {
+	console.log(counter);
+}*/
+
+/*====== Decrement ======*/
+
+/*var i;
+
+for (i = 10; i >= 0; i--) {
+	console.log(i);
+}*/
+
+/*====== Controlling the loop ======*/
+
+/*for (var a = 5; a <= 100; a+=5) {
+	console.log(a);
+}*/
+
+/*for (var a = 100; a >= 5; a-=5) {
+	console.log(a);
+}*/
+
+/*====== Loops meet arrays ======*/
+
+/*var junk = ["apple", "orange", 4, 6];
+console.log(junk);*/
+
+/*====== Array positions ======*/
+
+/*var junkData = ["Eddie Murphy", 49, "peanuts", 31];
+console.log(junkData[2]);*/
+
+/*====== Change array data ======*/
+
+/*var data = [1, 2, 3, 4, 5];
+console.log(data[3]);
+data[3] = "Mamba";
+console.log(data[3]);*/
+
+/*====== Loops and arrays I ======*/
+
+/*var cities = ["Aman", "New York", "Paris", "Plovdiv", "Hong Kong", "Tokyo"];
+
+for (i = 0; i < cities.length; i++) {
+	console.log("I would like to visit " + cities[i]);
+}*/
+
+/*====== Loops and arrays II ======*/
+
+/*var names = ["Ioan", "George", "Steve", "Jack", "Johnny"];
+
+for (i = 0; i < names.length; i++) {
+	console.log("I know someone called " + names[i]);
+}*/
+
+
+/*====== While loop - While syntax ======*/
+
+/*var understand = true;
+
+while (understand === true) {
+	console.log("I'm learning while loops!");
+	understand = false;
+}*/
+
+/*====== Brevity is the soul of programming ======*/
+
+/*var bool = true;
+
+while (bool) {
+	console.log("Less is more!");
+	bool = false;
+}*/
+
+/*====== Practice makes perfect ======*/
+
+/*var count = 0;
+
+var loop = function () {
+	while (count < 3) {
+		console.log("I'm looping!");
+		count++;
+	}
+};
+
+loop();*/
+
+/*====== Solo flight ======*/
+
+/*var loop = true;
+
+var soloLoop = function () {
+	while (loop) {
+		console.log("Looped once!");
+		loop = false;
+	}
+};
+
+
+soloLoop();*/
+
+
